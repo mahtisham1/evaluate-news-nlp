10c545b295b6d062ee3841164acab8b3bf3683e9
mahtisham1 <110836471+mahtisham1@users.noreply.github.com>
2022-09-09 18:51:59 +0500

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,56 @@
+import { isValidUrl } from './urlChecker'
+debugger
+const apiKey = '28f90c38c174f86b89a5ab005d134e30&units=imperial';
+
 function handleSubmit(event) {
     event.preventDefault()
+    debugger
+    let isValid = isValidUrl(document.getElementById('url').value)
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let baseURL = 'https://api.meaningcloud.com/sentiment-2.1'
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    getData(baseURL, apiKey)
+    .then(async function(data){
+      data = await postData('/add',{polarity: data.polarity, agreement: data.agreement, subjectivity: data.subjectivity, confidence: data.confidence, irony: data.irony});
+      updateUI(data)
     })
+  }
+
+  const getData = async (url,key)=>{
+    const res = await fetch(url+key)
+
+    try {
+      const data = await res.json();
+      console.log(data);
+
+      return data;
+    } catch(error) {
+      console.log("error", error);
+    }
+  }
+
+  const postData = async (url = '', data = {}) =>{
+    console.log(data)
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    return res.json()
+  }
+
+  const updateUI = (data) => {
+      document.getElementById('polarity').innerHTML = `polarity: ${data.date}`;
+      document.getElementById('agreement').innerHTML = `agreement: ${tempConvert}`;
+      document.getElementById('subjectivity').innerHTML = `subjectivity: ${data.city}`;
+      document.getElementById('confidence').innerHTML = `confidence: ${data.feelings}`;
+      document.getElementById('irony').innerHTML = `irony: ${data.feelings}`;
 }
 
 export { handleSubmit }
