f741dfefe1a90cd8d1e20009ebbbcbb01d6ba5b0
mahtisham1 <110836471+mahtisham1@users.noreply.github.com>
2022-09-19 20:53:34 +0500

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,17 @@
-import { checkForName } from './js/nameChecker'
+import { isValidUrl } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
+
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+// alert("I EXIST")
+// console.log("CHANGE!!");
 
 export {
-    checkForName,
+    isValidUrl,
     handleSubmit
 }
