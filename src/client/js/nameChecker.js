fed185aca0aee736bcf5a899c437000bb43eefa0
mahtisham1 <110836471+mahtisham1@users.noreply.github.com>
2022-09-19 20:45:41 +0500

    Update nameChecker.js

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,1 +1,11 @@
+    const isValidUrl = urlString=> {
+      try {
+          return (new URL(urlString));
+      }
+      catch(e){
+        alert("Please try with a valid URL");
+      }
+  }
 
+
+export { isValidUrl };
